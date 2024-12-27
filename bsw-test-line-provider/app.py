d8565e5e70589c5e83af25fdad9a45942a8a8c22
“Ruslan <akhmarovri@yandex.ru>
2024-12-27 16:04:53 +0300

    Line provider now sends messages to redis queue on event state updates

--- bsw-test-line-provider/app.py
+++ bsw-test-line-provider/app.py
@@ -1,76 +1,60 @@
-import decimal
-import enum
+import os
 import time
 from typing import Optional
 
 from fastapi import FastAPI, HTTPException, BackgroundTasks
-from pydantic import BaseModel
-import aio_pika
+import aioredis
 
-RMQ_URL = "amqp://rmuser:rmpassword@rabbitmq/"
-MAIN_QUEUE = "events"
-
-
-class EventState(enum.IntEnum):
-    NEW = 1
-    FINISHED_WIN = 2
-    FINISHED_LOSE = 3
+from common.models import Event, EventState
+from common.rqueue import RedisQueue
+from db import pool
 
 
-class Event(BaseModel):
-    event_id: str
-    coefficient: Optional[decimal.Decimal] = None
-    deadline: Optional[int] = None
-    state: Optional[EventState] = None
-
-
-events: dict[str, Event] = {
-    '1': Event(event_id='1', coefficient=1.2, deadline=int(time.time()) + 600, state=EventState.NEW),
-    '2': Event(event_id='2', coefficient=1.15, deadline=int(time.time()) + 60, state=EventState.NEW),
-    '3': Event(event_id='3', coefficient=1.67, deadline=int(time.time()) + 90, state=EventState.NEW)
+events: dict[int, Event] = {
+    1: Event(event_id=1, coefficient=1.2, deadline=int(time.time()) + 600, state=EventState.NEW),
+    2: Event(event_id=2, coefficient=1.15, deadline=int(time.time()) + 60, state=EventState.NEW),
+    3: Event(event_id=2, coefficient=1.67, deadline=int(time.time()) + 90, state=EventState.NEW)
 }
 
 
 app = FastAPI()
 
 
-async def events_publish(event: Event):
-    connection = await aio_pika.connect_robust(RMQ_URL)
-    data = event.model_dump_json(exclude_none=True)
+def get_redis() -> aioredis.Redis:
+    return aioredis.Redis(connection_pool=pool, decode_responses=True)
 
-    async with connection:
-        routing_key = MAIN_QUEUE
 
-        channel = await connection.channel()
-
-        await channel.default_exchange.publish(
-            aio_pika.Message(body=data.encode()),
-            routing_key=routing_key,
-        )
+async def send_msg(event_id: str) -> None:
+    rqueue = RedisQueue(get_redis(), os.getenv("REDIS_QUEUE_EVENTS_POSTFIX", "events"))
+    await rqueue.publish(event_id)
 
 
 @app.put('/events')
-async def create_event(event: Event, background_tasks: BackgroundTasks):
+async def create_event(event: Event, background_tasks: BackgroundTasks) -> Event:
+    if event.state in (EventState.FINISHED_WIN, EventState.FINISHED_LOSE):
+        background_tasks.add_task(send_msg, str(event.event_id))
+
     if event.event_id not in events:
         events[event.event_id] = event
-        background_tasks.add_task(events_publish, event)
-        return {}
+        return event
 
-    background_tasks.add_task(events_publish, event)
     for p_name, p_value in event.dict(exclude_unset=True).items():
         setattr(events[event.event_id], p_name, p_value)
 
-    return {}
+    return events[event.event_id]
 
 
 @app.get('/events/{event_id}')
-async def get_event(event_id: str):
+async def get_event(event_id: int) -> Event:
     if event_id in events:
         return events[event_id]
 
     raise HTTPException(status_code=404, detail="Event not found")
 
 
-@app.get('/events')
-async def get_events():
-    return list(e for e in events.values() if time.time() < e.deadline)
+@app.get('/events/')
+async def get_events(archive: Optional[int] = None) -> list[Optional[Event]]:
+    if archive:
+        return list(e for e in events.values())
+    else:
+        return list(e for e in events.values() if time.time() < e.deadline)
